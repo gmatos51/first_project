45c30b2b7d3b03dc4699d80ea26107a2eb9c997b
GonÃcalo Matos <goncalormatos333@gmail.com>
2021-08-30 15:39:15 +0100

    Initial Push

---
+++ first_project/lib.py
@@ -0,0 +1,9 @@
+# mlproject/lib.py
+
+def try_me():
+    while True:
+        best_player = input('Who is the GOAT of football? ')
+        if best_player.lower() != 'ronaldo':
+            print('Nope! Try again. (tip: it\'s portuguese and try to write only one name)')
+        else:
+            return 'SIIIIIIIIIIIIIIIIII'
