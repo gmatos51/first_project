fd15df9ac2542f118e27785cb4d0f684ccb5d63d
GonÃcalo Matos <goncalormatos333@gmail.com>
2021-08-30 16:04:26 +0100

    2nd push

--- first_project/lib.py
+++ first_project/lib.py
@@ -1,9 +1,11 @@
 # mlproject/lib.py
 
 def try_me():
+
+    lst = ['ronaldo', 'cristiano', 'cristiano ronaldo']
     while True:
         best_player = input('Who is the GOAT of football? ')
-        if best_player.lower() != 'ronaldo':
+        if best_player.lower() not in lst:
             print('Nope! Try again. (tip: it\'s portuguese and try to write only one name)')
         else:
             return 'SIIIIIIIIIIIIIIIIII'
